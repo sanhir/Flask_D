074bf25aa8a100bc695246e38ae2344699270e3a
KawakamiNijiho <kawakami.nijiho.qm@mynavi.jp>
2023-06-01 06:28:46 +0000

    17章まで

--- kawakami/application/flask_blog/views.py
+++ kawakami/application/flask_blog/views.py
@@ -1,8 +1,26 @@
 # flaskのパッケージをインポート
 from flask import request,redirect,url_for,render_template,flash,session
-
 from flask_blog import app
 
 @app.route('/')
 def show_entries():
     return render_template('entries/index.html')
+
+# /loginのURLにリクエストがあったときのルーティング処理
+
+@app.route('/login',methods = ['GET','Post'])
+def login():
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            print('ユーザー名が異なります')
+        elif request.form['password'] != app.config['PASSWORD']:
+            print('パスワードが異なります')
+        else:
+            return redirect('/')
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    return redirect('/')
+    
+        
