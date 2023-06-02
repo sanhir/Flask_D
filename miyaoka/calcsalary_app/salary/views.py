c2d7bafd26b9dd0529fdfc6407c98f04022e04a4
TakumiMiyaoka <miyaoka.takumi.qo@mynavi.jp>
2023-06-02 04:01:47 +0000

    おい、給料あげろ.exe

--- miyaoka/calcsalary_app/salary/views.py
+++ miyaoka/calcsalary_app/salary/views.py
@@ -4,23 +4,30 @@ from decimal import Decimal, ROUND_HALF_UP
 
 @app.route('/')
 def show_entries():
-    # if not session.get('logged_in'):
-    #     return redirect(url_for('templates/input.html'))
     return render_template('input.html')
 
 @app.route('/output',methods=['GET','POST'])
 def logout():
-    # session.pop('logged_in',None)
-    # flash('ログアウトしました')
-    salary = int(request.form['salary'])
-    if salary >= 1000000:
-         tax = 100000 + (salary-1000000) * 0.2
+    if request.form['salary'] == "":
+        flash('おい、給料入れろよ')
+        return redirect(url_for('show_entries'))
     else:
-        tax = salary * 0.1
-        #税額を四捨五入
-        tax = Decimal(str(tax)).quantize(Decimal("0"),rounding=ROUND_HALF_UP)
-    
-    #支給額を計算
-    after_tax_pay = salary - tax
+        if len(request.form['salary']) > 10:
+            flash('給料高すぎ')
+            return redirect(url_for('show_entries'))
+        salary = int(request.form['salary'])
+        if salary < 0:
+            flash('給料はもらうもの')
+            return redirect(url_for('show_entries'))
+        
+        if salary >= 1000000:
+            tax = 100000 + (salary-1000000) * 0.2
+        else:
+            tax = salary * 0.1
+            #税額を四捨五入
+            tax = Decimal(str(tax)).quantize(Decimal("0"),rounding=ROUND_HALF_UP)
+        
+        #支給額を計算
+        after_tax_pay = salary - tax
 
-    return render_template("output.html", salary=salary, tax=tax, after_tax_pay=after_tax_pay)
+        return render_template("output.html", salary=salary, tax=tax, after_tax_pay=after_tax_pay)
