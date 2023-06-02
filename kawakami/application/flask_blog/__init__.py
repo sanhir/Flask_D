9f2b31372f505815561b55f878c90ca18c7141a1
KawakamiNijiho <kawakami.nijiho.qm@mynavi.jp>
2023-06-02 06:46:01 +0000

    24章まで

--- kawakami/application/flask_blog/__init__.py
+++ kawakami/application/flask_blog/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 # flask_blogフォルダ以下のcongfig.pyの内容をconfigとして扱う
 app.config.from_object('flask_blog.config')
+
+db = SQLAlchemy(app)
 
 import flask_blog.views
