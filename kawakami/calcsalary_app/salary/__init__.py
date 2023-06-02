7044a7a07834f88eb3f41046a9f7a7eef4274a6a
KawakamiNijiho <kawakami.nijiho.qm@mynavi.jp>
2023-06-02 04:29:44 +0000

    給与計算アプリ

--- kawakami/calcsalary_app/salary/__init__.py
+++ kawakami/calcsalary_app/salary/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 # flask_blogフォルダ以下のcongfig.pyの内容をconfigとして扱う
-app.config.from_object('flask_blog.config')
+app.config.from_object('salary.config')
 
-import salary.views
+import salary.views.views
