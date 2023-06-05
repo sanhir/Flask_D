84226786804091187ee964f4899b024dce1452b2
SeiyaYoshida515 <yoshida.seiya.ay@mynavi.com>
2023-06-05 01:58:41 +0000

    32章まで

--- yoshida/application/flask_blog/views/views.py
+++ yoshida/application/flask_blog/views/views.py
@@ -1,7 +1,19 @@
 from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
+from functools import wraps
+from flask import Blueprint
 
-@app.route("/login", methods=["GET", "POST"])
+view = Blueprint('view', __name__)
+
+def login_required(view):
+    @wraps(view)
+    def inner(*args, **kwargs):
+        if not session.get("logged_in"):
+            return redirect(url_for('view.login'))
+        return view(*args, **kwargs)
+    return inner
+
+@view.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         if request.form["username"] != app.config["USERNAME"]:
@@ -11,11 +23,15 @@ def login():
         else:
             session["logged_in"] = True
             flash("ログインしました")
-            return redirect(url_for("show_entries"))
+            return redirect(url_for("entry.show_entries"))
     return render_template("login.html")
 
-@app.route("/logout")
+@view.route("/logout")
 def logout():
     session.pop("logged_in", None)
     flash("ログアウトしました")
-    return redirect(url_for("show_entries"))
+    return redirect(url_for("entry.show_entries"))
+
+@view.app_errorhandler(404)
+def non_existant_route(error):
+    return redirect(url_for('view.login'))
