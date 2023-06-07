9fba1c4201eeb1c4e7fc29b2c6e39971c82bd476
TakumiMiyaoka <miyaoka.takumi.qo@mynavi.jp>
2023-06-07 02:45:48 +0000

    祝日マスターにかかれば明日を祝日にすることも容易である

--- miyaoka/holiday_app/holiday/models/entries.py
+++ miyaoka/holiday_app/holiday/models/entries.py
@@ -2,13 +2,13 @@ from holiday import db
 from datetime import datetime
 
 class Entry(db.Model):
-    __tablename__ = 'holiday'
+    __tablename__ = 'holidate'
     holi_date = db.Column(db.DateTime, primary_key=True)
     holi_text = db.Column(db.String(20), unique=True)
 
     def __init__(self, holi_date=None, holi_text=None):
-        self.date = holi_date
-        self.text = holi_text
+        self.holi_date = holi_date
+        self.holi_text = holi_text
 
     def __repr__(self):
-        return '<Entry id:{} title:{} text:{}>'.format( self.title, self.text)
+        return '<Entry date:{} text:{}>'.format( self.date, self.text)
